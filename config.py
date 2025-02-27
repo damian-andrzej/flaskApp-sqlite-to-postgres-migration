dd69423a78181dd1c1437665bc7ffa730fc477ef
damian-andrzej <102800704+damian-andrzej@users.noreply.github.com>
2025-02-27 19:40:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # SQLite database for simplicity
-    SQLALCHEMY_DATABASE_URI ='postgresql://postgres:admin@localhost/flask_db'
+    SQLALCHEMY_DATABASE_URI ='postgresql://admin:mypassword@db:5432/flask_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
